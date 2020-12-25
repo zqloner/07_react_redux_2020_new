d1284843f1dfab850b3c46c99ad5f3c3cb61cf8c
zhangqi <zqloner@163.com>
2020-12-25 13:05:01 +0800

    03_src_redux完整版

---
+++ 03_src_redux完成版/components/Count/index.jsx
@@ -0,0 +1,58 @@
+import React,{Component} from "react";
+//引入store,用于获取redux中保存的状态
+import store from '../../redux/store'
+//引入actionCreator,专门用于创建action对象
+import {createIncrementAction,createDecrementAction} from "../../../src/redux/count_action";
+
+export default class Count extends Component {
+  increment = ()=>{
+    const {value} = this.selectNumber
+    //通知redux加value值
+    store.dispatch(createIncrementAction(value*1))
+  }
+  decrement = ()=>{
+    const {value} = this.selectNumber
+    //通知redux加value值
+    store.dispatch(createDecrementAction(value*1))
+  }
+  crementIfOdd = ()=>{
+    const {value} = this.selectNumber
+    const count = store.getState()
+    if(count % 2 !==0){
+      store.dispatch(createIncrementAction(value*1))
+    }
+  }
+  crementIfAsync = ()=>{
+    const {value} = this.selectNumber
+    setTimeout(()=>{
+      store.dispatch(createIncrementAction(value*1))
+      },1000)
+  }
+  //检测redux中状态的变化,只要变化,就调用render。
+  // componentDidMount() {
+  //   //一挂载就检测redux中状态的变化,只要变化,就调用render。
+  //   store.subscribe(()=>{
+  //     console.log("检测store中状态的变化的回调")
+  //     //render()方法是不能通过this.render()方法调用的。没有用
+  //     //但是需要render更新view。所以可以用
+  //     this.setState({})
+  //   })
+  // }
+
+  render() {
+    return (
+      <div>
+          <h1>当前求和为:{store.getState()}</h1>
+        <select ref={c=>this.selectNumber = c}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>&nbsp;&nbsp;
+        <button onClick={this.increment}>+</button>&nbsp;&nbsp;
+        <button onClick={this.decrement}>-</button>&nbsp;&nbsp;
+        <button onClick={this.crementIfOdd}>当前求和为奇数再加</button>&nbsp;&nbsp;
+        <button onClick={this.crementIfAsync}>异步加</button>&nbsp;&nbsp;
+      </div>
+    );
+  }
+}
